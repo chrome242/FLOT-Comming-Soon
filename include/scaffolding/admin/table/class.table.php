16578e3b9256ae2f65c85af86f578d26bc85432f
William LaMorie <wjl10@cornell.edu>
2015-05-08 20:57:11 -0400

    added attribs, helper method
    -added the attributes for the class
    -added makeFormat() to process the header array
    -made initial constructor method

--- include/scaffolding/admin/table/class.table.php
+++ include/scaffolding/admin/table/class.table.php
@@ -12,7 +12,7 @@
  *
  * open and close a table structure
  *
- * set the class, id, name, et al of the table.
+ * set the class, id, name, action, et al of the table.
  * 
  * take an array of header names to make a header and row rules
  * the header array should be in the form of array(header => array(cell => rule))
@@ -22,12 +22,17 @@
  * take an array of format: row name => array(cell name => values)
  *  -use that array to generate a list of rows with cell named cells
  *  -alternate between sets of rows if need be
- *  -set some rows to hidden, allow them to be shown if need be
  *  -use a set of setters to set values for all rows of given type
  *  -use a set of getters to access cells from either the exposed or
  *   the hidden cells of a given row.
  *  -include a task specific method to set some cell values based on other
  *   cell values, applying this to all rows.
+ *   
+ * allow the creation of an 'update' button to post to defined page
+ * set up the name of the form from the table name
+ *  
+ * because there's a lot of customization in this class, it will need a number
+ * of private methods for the toString method.
  *
  * indivudual rows should be accessed via getters and setters, and while it
  * shouldn't be used much, it should be resonable to use a row setter to
@@ -38,15 +43,60 @@
 class Table {
   
   // class attributes
-  protected $_name;
+  protected $_name; // the form name.
   protected $_id = null;
   protected $_class = null;
   
   // member portion attributes
-  protected $_header;
-  protected $_rows;
-  protected $_privateRows;
+  protected $_header; // takes the header key
+  protected $_format; // takes the header array value
+  protected $_rows;  // the row array
   
+  /**
+   * Constructor
+   *
+   * a basic table & form constructor.
+   *
+   * @param str $name the name of the form
+   * @param mixed array $rows array of row name => (cell value array)
+   * @param mixed array $header array of header name => array(cell name => format)
+   *
+   * due to radio buttons, some of the valyes in the header array may not be
+   * name => array but rather name => int where the in is just a placeholder
+   * for keeping track of where in the radio set the table is
+   */
+  public function __construct($name, $rows, $header){
+    $this->_name = $name;
+    $this->_header = array_keys($header);
+    $this->_format = $this->makeFormat($header);
+    $this->_rows = $this->makeRows($rows, $this->_format);
+    
+  }
   
+  /**
+   * a method for some array foo.
+   *
+   * takes an array of the form old key => either:
+   * new key => value or an int (for a drop)
+   * drops all values where the old key => int, and makes a new array of
+   * the form key => value from the old values.
+   *
+   * @param mixed array $raw_array: an array of the form above
+   *
+   * @return array $output: the new array
+   */
+  private function makeFormat($raw_array){
+    // setup the return array
+    $output = array();
+    
+    //itterate though the raw array because of mixed types
+    foreach($raw_array as $key => $value){
+      if(gettype($value) == "array"){ $output[$value[0]] = $value[1]; }
+    }
+    
+    return $output;
+  }
+  
+  private function makeRows($rows, $format){}
   
 }
