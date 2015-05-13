5a5fa6e57a300bee231d24af322a79468d2bec9c
William LaMorie <wjl10@cornell.edu>
2015-05-13 08:10:47 -0400

    readability change
    -rectified two commit streams
    -improved table.php readability

--- include/scaffolding/admin/table/table.php
+++ include/scaffolding/admin/table/table.php
@@ -1,14 +1,20 @@
 <?php
-
+// table class
 include_once("class.table.php");
+
+// row class
 include_once("class.row.php");
+
+
 // text cells
 include_once("class.cell.php"); // basic cell class
 include_once("class.cell.timestamp.php"); // timestamp extension for cell class
+
 // input cells
 include_once("class.cell.input.php"); // the input interace for cells
 include_once("class.cell.checkbox.php"); //the checkbox cell type
 include_once("class.cell.radio.php"); // the radio cell type
+
 
 // testing data
 include_once("test.data.php");
