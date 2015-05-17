4a969337bae18da8581ae19df36171fcc766c236
William LaMorie <wjl10@cornell.edu>
2015-05-17 09:05:21 -0400

    notes, roadpath (nb)
    -added some notes on what functions remain
    -added some notes on the coming JS

--- include/scaffolding/admin/table/table.php
+++ include/scaffolding/admin/table/table.php
@@ -1,4 +1,7 @@
 <?php
+// panel class
+//include_once("class.panel.php");
+
 // table class
 include_once("class.table.php");
 
@@ -9,6 +12,7 @@ include_once("class.row.php");
 // text cells
 include_once("class.cell.php"); // basic cell class
 include_once("class.cell.timestamp.php"); // timestamp extension for cell class
+//include_once("class.cell.button.php"); // in-row button
 
 // input cells
 include_once("class.cell.input.php"); // the input interace for cells
@@ -17,5 +21,21 @@ include_once("class.cell.radio.php"); // the radio cell type
 include_once("class.cell.text.php"); // the text cell type
 include_once("class.cell.number.php"); // the numer cell type
 
+// input like cells, use the input interface
+//include_once("class.cell.textarea.php"); //text area input
+//include_once("class.cell.select.php"); // select box
+
+// cells with mutiple elements
+//include_once("class.cell.extras.php");
+
 // testing data
 include_once("test.data.php");
+
+// javaScript that needs to be done to support the above- the ones marked with
+// an <a> are ajax calls and should have supporting php (mostly above)
+// lock-button - lock edit button
+// replace-row - replace an edit/not edit row with the other <a>
+// replace-cell -support the above, used for extras and dishes <a>
+// add-cell -to add a cell to the table <a>
+// add-row -to add a row to the table
+
