8747928a6982d3949c638c64c1552780d7114d38
William LaMorie <wjl10@cornell.edu>
2015-06-02 18:11:23 -0400

    Layed out the drink management page
    -layed out the drink management page.

--- Admin/Drinks/index.php
+++ Admin/Drinks/index.php
@@ -1,109 +1,72 @@
-<?php
-include("../../include/config.php");
-include(SCAFFOLDING_ADMIN."menubars.php");
-include(SCAFFOLDING_ADMIN."list/class.listView.php");
-include(SCAFFOLDING_ADMIN."panel/class.panel.php");
-include(SCAFFOLDING_ADMIN."table/table.php");
 
+<?php
+
+include("../include/config.php");
+include(SCAFFOLDING_ADMIN."admin.include.php"); // centeralized admin includes
+
+// TODO: Add a test login to redirect function here
+// if(!secureCheckLogin($_COOKIE)){header:Admin Login}
+  // check to make sure the user is logged in
+    // if logged in, check to make sure the token is valid
+    // if not logged in, send to the Admin login page
+  // if no admin access token, send to Main site
+
+// TODO: Process the user's permission array from the session
+// $permissions = $_COOKIE["admin_permissions"];
+
+// Page variables
+
+                 
 $title = "Manage Drinks";
-$root = ADMIN;
 $section = ADMIN."Drinks/";
 
-$permissions = array("inventory" => "1",
-                     "drinks" =>  "1",
-                     "extras" => "1",
-                     "food" => 1,
-                     "add_user" => "1",
-                     "edit_user" => 1);
+$sectionWrappers = array("beerManagement" => ["Beer Management", false],
+                         "wineManagement" => ["Wine Management", false],
+                         "breweryManagement" => ["Brewery Management", false],
+                         "wineryManagement" => ["Winery Management", false]);
 
-$cells = array("Id" => '3',
-               "Thing" => "Thing 1",
-               "Another Thing" => "Thing 2",
-               "Checkmeout" => False,
-               "RadioGaGa" => 3,
-               "TimeandSpace" => time(),
-               "Blurb" => "Some Text",
-               "Count" => '3.26666',
-               "TextArea" => "A house, in the hills",
-               "ASelect" => $permissions);
+// variables for sections:
+$optionsBeer = array("inhouse" => "In House",
+                 "ondeck" => "On Deck",
+                 "kicked" => "Kicked",
+                 "all" => "All");
 
-$format = array("Id" => 'id',
-                "Thing" => "plain",
-                "Another Thing" => "plain",
-                "Checkmeout" => "checkbox",
-                "RadioGaGa" => "radio, 6",
-                "TimeandSpace" => "time, private",
-                "Blurb" => "text, placeholder",
-                "Count" => "number, value, 4, 8",
-                "TextArea" => "textarea, value",
-                "ASelect" => "select");
+$optionsWine = array("instock" => "In Stock",
+                 "outofstock" => "Out Of Stock",
+                 "WineAll" => "All");
 
-
-function testWrap($str){
-  $array = explode("\n", $str);
-  $output = '';
-  foreach($array as $line){
-    $output .= "  " . $line ."\n";
-  }
-  return $output;
-}
-
-
+// open the page
 include(SCAFFOLDING."head.php");
 
+ // Menu Bar
 echo menubar($permissions, $section, $root);
 
-echo '
-        <div class="container">
-          <form name="beerManagement" method="post">
-            <table class="table table-hover" id="beer">
-              <thead>';
+// section bar
+echo sectionbar($sectionWrappers);
+
+// beer management wrapper
+echo '      <div class="collapse" id="beerManagement"> <!-- Beers -->';
+echo sortbar($optionsBeer, 'all');
+// TODO: make handler for beer table
+echo '      </div>';
 
 
-$test1 = new Text("Test", "Some test content", $type="text");
-$test2 = new Number("Num Test", 4.00, "placeholder", 2, 5);
+// wine management wrapper
+echo '      <div class="collapse" id="wineManagement"> <!-- Wines -->';
+echo sortbar($optionsWine, 'WineAll');
+// TODO: make handler for wine table
+echo '      </div>';
 
-$test_text = "The Quick Brown Fox Jumped Over the Lazy Dog.";
-$test3 = new Textarea("TestArea", $test_text, "placeholder");
-$test4 = new Select("SelectTest", $format, "Id", true, 1);
-$test0 = new NewRow();
-$test5 = new Duration("Test", (time() - (2*24*60*60)), (time() - (2*24*60*60)));
-$test6 = new Text("test2333", "some text");
-$test7 = new Button("form", "Something Good", "submit");
-$test8 = new Cell("ButtonCell", "Look");
-$test1->addButton("form", "record", "action", "btntxt", $text=true, $active=false);
-$test4->addButton("form", "record", "anotheraction", "moarbtn", $text=true, $active=false);
-//$test1->controlOff();
-//$test1->disabled();
-echo $test1;
-echo $test2;
-echo $test3;
-//echo $test4;
-//echo $test0;
-//echo $test8;
-echo $test5;
-echo $test6;
-$thing =  testWrap($test4);
-echo $thing;
-echo $test7;
+// brewery management wrapper
+echo '      <div class="collapse" id="breweryManagement"> <!-- Breweries -->';
+// TODO: make handler for brewery table
+echo '      </div>';
 
-$strtest = '
-123456789
-123456789
-123456789';
+// winery management wrapper
+echo '      <div class="collapse" id="wineryManagement"> <!-- Breweries -->';
+// TODO: make handler for brewery table
+echo '      </div>';
 
-echo $strtest;
-echo $strtest;
-echo testWrap($strtest);
 
-$test3 = new Row("beer", $cells, $format);
-$test3->setCellClass("Blurb", "col-xs-2");
-echo $test3;
-
-echo "<br><pre>";
-if(isset($_POST)){
-  var_dump($_POST);
-}
-echo "</pre>";
-
+// close the page
 include(SCAFFOLDING_ADMIN."footer.php");
--- Admin/index.old.php
+++ Admin/index.old.php
@@ -64,3 +64,114 @@ echo $testSmall;
 
 
 include(SCAFFOLDING_ADMIN."footer.php");
+
+
+<?php
+include("../../include/config.php");
+include(SCAFFOLDING_ADMIN."menubars.php");
+include(SCAFFOLDING_ADMIN."list/class.listView.php");
+include(SCAFFOLDING_ADMIN."panel/class.panel.php");
+include(SCAFFOLDING_ADMIN."table/table.php");
+
+$title = "Manage Drinks";
+$root = ADMIN;
+$section = ADMIN."Drinks/";
+
+$permissions = array("inventory" => "1",
+                     "drinks" =>  "1",
+                     "extras" => "1",
+                     "food" => 1,
+                     "add_user" => "1",
+                     "edit_user" => 1);
+
+$cells = array("Id" => '3',
+               "Thing" => "Thing 1",
+               "Another Thing" => "Thing 2",
+               "Checkmeout" => False,
+               "RadioGaGa" => 3,
+               "TimeandSpace" => time(),
+               "Blurb" => "Some Text",
+               "Count" => '3.26666',
+               "TextArea" => "A house, in the hills",
+               "ASelect" => $permissions);
+
+$format = array("Id" => 'id',
+                "Thing" => "plain",
+                "Another Thing" => "plain",
+                "Checkmeout" => "checkbox",
+                "RadioGaGa" => "radio, 6",
+                "TimeandSpace" => "time, private",
+                "Blurb" => "text, placeholder",
+                "Count" => "number, value, 4, 8",
+                "TextArea" => "textarea, value",
+                "ASelect" => "select");
+
+
+function testWrap($str){
+  $array = explode("\n", $str);
+  $output = '';
+  foreach($array as $line){
+    $output .= "  " . $line ."\n";
+  }
+  return $output;
+}
+
+
+include(SCAFFOLDING."head.php");
+
+echo menubar($permissions, $section, $root);
+
+echo '
+        <div class="container">
+          <form name="beerManagement" method="post">
+            <table class="table table-hover" id="beer">
+              <thead>';
+
+
+$test1 = new Text("Test", "Some test content", $type="text");
+$test2 = new Number("Num Test", 4.00, "placeholder", 2, 5);
+
+$test_text = "The Quick Brown Fox Jumped Over the Lazy Dog.";
+$test3 = new Textarea("TestArea", $test_text, "placeholder");
+$test4 = new Select("SelectTest", $format, "Id", true, 1);
+$test0 = new NewRow();
+$test5 = new Duration("Test", (time() - (2*24*60*60)), (time() - (2*24*60*60)));
+$test6 = new Text("test2333", "some text");
+$test7 = new Button("form", "Something Good", "submit");
+$test8 = new Cell("ButtonCell", "Look");
+$test1->addButton("form", "record", "action", "btntxt", $text=true, $active=false);
+$test4->addButton("form", "record", "anotheraction", "moarbtn", $text=true, $active=false);
+//$test1->controlOff();
+//$test1->disabled();
+echo $test1;
+echo $test2;
+echo $test3;
+//echo $test4;
+//echo $test0;
+//echo $test8;
+echo $test5;
+echo $test6;
+$thing =  testWrap($test4);
+echo $thing;
+echo $test7;
+
+$strtest = '
+123456789
+123456789
+123456789';
+
+echo $strtest;
+echo $strtest;
+echo testWrap($strtest);
+
+$test3 = new Row("beer", $cells, $format);
+$test3->setCellClass("Blurb", "col-xs-2");
+echo $test3;
+
+echo "<br><pre>";
+if(isset($_POST)){
+  var_dump($_POST);
+}
+echo "</pre>";
+
+include(SCAFFOLDING_ADMIN."footer.php");
